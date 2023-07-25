a499b5d4d7b4b32bb4772f65a77bc52a7d920f62
LynneDC <roselinedc1308@gmail.com>
2023-07-25 18:54:03 +0200

    buble sort algorithm in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,6 @@
 #include "sort.h"
-#include <stdbool.h>
 
-/**
- * swap -k swaps integers in ascending arrangement
- * @xp: ponter
- * @yp: pointer
- */
 
-void swap(int *xp, int *yp)
-{
-int temp = *xp;
-*xp = *yp;
-*yp = temp;
-}
 /**
  * bubble_sort - sort an array of int in order usin bubble  sort algorithm
  *  @array: pointer to array to sort
@@ -20,26 +8,23 @@ int temp = *xp;
  */
 void bubble_sort(int *array, size_t size)
 {
-int i, j;
+int i, j, temp = 0;
 
-bool swapped;
+if (size < 2)
+return;
+
 for (i = 0; i < (int)size - 1; i++)
 {
-swapped = false;
+
 for (j = 0; j < (int)size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-swap(&array[j], &array[j + 1]);
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
 print_array(array, size);
-swapped = true;
 }
 }
-
-/* If no two elements were swapped by inner loop,*/
-/* then break*/
-if (swapped == false)
-break;
 }
 }
-
