70ec4a8fca4b6e5295fbd5ec8d1151731dbdfce6
LynneDC <roselinedc1308@gmail.com>
2023-07-25 19:43:14 +0200

    buble sort algorithm in ascending order

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,9 +13,9 @@ void bubble_sort(int *array, size_t size)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
